ce0afc127a2f0fa4fcea822ac33b0a846254be36
ErikEverton <122756143+ErikEverton@users.noreply.github.com>
2024-01-10 21:50:31 +0000

    I forget git add in the last commit

--- linked_list.py
+++ linked_list.py
@@ -5,7 +5,40 @@ class Node:
     
 
 class LinkedList:
-    def __init__(self) -> None:
-        
+    def __init__(self, head) -> None:
+        first_node = Node(head)
+        self.head = first_node
+    
+    def insert_at_the_beginning(self, value):
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
+    
+    def delete(self, value):
+        current = self.head
 
-print(node.value, node.next)
+        if value == self.head.value:
+            self.head = self.head.next
+
+        while current:
+            next = current.next
+            if next:
+                if next.value == value:
+                    current.next = next.next
+                else:
+                    current = next
+            current = next
+
+
+list = LinkedList(0)
+
+for n in range(1, 5):
+    list.insert_at_the_beginning(n)
+
+list.delete(2)
+
+current = list.head
+
+while current:
+    print(current.value)
+    current = current.next
