8bad5757d09bba6a8feece38cc1f7c8a22375b81
ErikEverton <122756143+ErikEverton@users.noreply.github.com>
2024-01-10 21:43:47 +0000

    This is the basic of what i want to do.

---
+++ linked_list.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next = None
+    
+
+class LinkedList:
+    def __init__(self) -> None:
+        
+
+print(node.value, node.next)
