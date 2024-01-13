6bd32c7b4863511530a911dad681dd0113ab7cee
Erik Everton Rodrigues <erik.everton444@gmail.com>
2024-01-13 19:34:28 -0300

    stack isEmpty method added

--- stack.py
+++ stack.py
@@ -5,3 +5,15 @@ class Node:
     
     def __repr__(self):
         return f'{self.value}, {self.next}'
+
+
+class Stack:
+    def __init__(self) -> None:
+        self.top = None
+    
+    def isEmpty(self, value):
+        if self.top is None:
+            return True
+        return False
+    
+    
