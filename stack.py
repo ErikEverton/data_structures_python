547910e3f84f6d138c15261a765c4b573abe7fc1
Erik Everton Rodrigues <erik.everton444@gmail.com>
2024-01-13 19:50:15 -0300

    stack pop and peek methods added

--- stack.py
+++ stack.py
@@ -9,7 +9,7 @@ class Node:
 
 class Stack:
     def __init__(self) -> None:
-        self.top = Node()
+        self.top = None
     
     def isEmpty(self):
         if self.top.value is None:
@@ -21,6 +21,28 @@ class Stack:
         new_node.next = self.top
         self.top = new_node
     
+    def pop(self):
+        last_top = self.top
+        self.top = self.top.next
+        return last_top.value
+
+    def peek(self):
+        return self.top
 
     def __repr__(self) -> str:
         return f'[{self.top.value}, {self.top.next}]'
+    
+
+
+# Some tests
+    
+stack = Stack()
+
+for n in range(0, 10):
+    stack.push(n)
+
+for n in range(0, 5):
+    stack.pop()
+
+print(stack.pop())
+print(stack)
