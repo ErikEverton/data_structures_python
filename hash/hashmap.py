d428cf74d2cd92c104d05e1fe74e2c98a549e1bd
Erik Everton Rodrigues <erik.everton444@gmail.com>
2024-01-14 18:11:05 -0300

    HashMap get method added

--- hash/hashmap.py
+++ hash/hashmap.py
@@ -17,6 +17,17 @@ class HashMap(object):
             self.data[address] = []
         self.data[address].append(key, value)
         return self.data
+    
+    def get(self, key):
+        address = self._hash(key)
+        currentBucket = self.data[address]
+
+        if currentBucket:
+            for i in range(len(currentBucket)):
+                if currentBucket[i][0] == key:
+                    return currentBucket[i][1]
 
     def __str__(self) -> str:
         return "".join(str(item) for item in self.data)
+    
+
